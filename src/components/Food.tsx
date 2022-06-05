6df07fd5280d818f1a208b9b511c9ea10ffe8631
Vishkujo <vepicage@gmail.com>
2022-06-05 17:26:30 -0400

    Updating menu

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -1,40 +1,28 @@
 type FoodProps = {
   food: {
-      food_id: String,
-      food_name: String,
-      price: String,
-      description: String,
-      category: String,
-      active: String,
-      image: String
+    food_id: String,
+    food_name: String,
+    price: String,
+    description: String,
+    category: String,
+    active: String,
+    image: String
   };
 };
 
 const Food = ({ food }: FoodProps) => {
   return (
       <div className='food-box'>
-         <div className='foodpic'>
-         <img src={`https://shielded-depths-40144.herokuapp.com/assets/images/${food.image}`} alt={`${food.food_name}`} className='img-responsive img-curve' height="auto" width="150px"/>
-         </div>
+        <div className='food-img'>
+          <img src={`https://shielded-depths-40144.herokuapp.com/assets/images/${food.image}`} alt={`${food.food_name}`} className="img-responsive img-curve" height="auto" width="150px" />
+        </div>
+        <div className='food-details'>
+          <h4>{food.food_name}</h4>
+          <p className="food-price">${food.price}</p>
+          <p className="food-desc">{food.description}</p>
+        </div>
       </div>
   );
 };
 
 export default Food;
-
-{/* <table>
-              <thead>
-                  <tr>
-                      <th colSpan={3}>{food.food_name}</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr>
-                      <td>
-                          <img src={`https://shielded-depths-40144.herokuapp.com/assets/images/${food.image}`} />
-                      </td>
-                      <td>{food.description}</td>
-                      <td className="right">Price: ${food.price}</td>
-                  </tr>
-              </tbody>
-          </table> */}
