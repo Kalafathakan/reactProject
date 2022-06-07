66ae8c69c76d7e0caf912d7b0bd7ed68badf2553
Oreoluwatemi <temitayolawal35@gmail.com>
2022-06-07 14:43:28 -0400

    Designed Contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles/Contact.css'
+import '../images/res.jpeg';
 
 type Props = {};
 
@@ -9,12 +11,29 @@ const Contact = (props: Props) => {
     navigate('/');
   };
   return (
-    <div className='page-style'>
-      <h1>Contact Us</h1>
-      <p>Please feel free to send an email for bookings and other inquiries</p>
-      <a href = "mailto:kalafatzaferhakan@gmail.com">Send Email</a>
-      <br></br><hr></hr>
-      <button onClick={handleClick}>Go home</button>
+    <div className="container-form">
+      <div className="t-a">
+        <h2>Contact Us</h2>
+        <p>Swing by for a cup of coffee, or feel free to leave us a message:</p>
+      </div>
+      <div className="row-form">
+        <div className="column-form">
+          <img id="contact-img" src={require('../images/res.jpeg')} />
+        </div>
+        <div className="column-form">
+          <form>
+            <label htmlFor="fname">First Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+            <label htmlFor="lname">Last Name</label>
+            <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Your Email.." />
+            <label htmlFor="subject">Subject</label>
+            <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
