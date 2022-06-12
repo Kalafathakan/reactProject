0d3c82c30fbdba1b860345204cd85e729a795759
Nicholas Keller-Sedan <104579750+NKellerSedan@users.noreply.github.com>
2022-06-11 20:08:30 -0400

    updated deals page with image

--- src/pages/Deals.tsx
+++ src/pages/Deals.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles/Deals.css'
 
 type Props = {};
 
 const Deals = (props: Props) => {
-/*   const navigate = useNavigate();
+  const navigate = useNavigate();
   const handleClick = () => {
     navigate('/');
-  }; */
+  };
   return (
     <div className='page-style'>
-      <h1>Deals & Promotions</h1>
-
-      <span>
-        <h4><u>Images coming soon!</u></h4>
-        <ol>
-          <li>Monday: <strong>Lunch Deal!!</strong> Tacos and a Pint for <strong>$12.99</strong></li>
-          <li>Friday: <strong>End of Week Deal!!</strong> Tacos and a Pint for <strong>$12.99</strong></li>
-          <li>After 8pm every night: Nachos only <strong>$5.99</strong> with purchase of a drink!</li>
-        </ol>
-      </span>
+      <div className='div-img'>
+        <h1>Deals & Promotions</h1>
+        <img src={require('../images/dealsAndPromotions.jpeg')}></img>
+      </div>
     </div>
   );
 };
